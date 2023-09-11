62cb99c5697b6177c1b5bc137372876db3e93460
Alex Schubert <alexander.schubert85@gmail.com>
2023-09-10 17:34:26 -0700

    fixed number_of_ways
    changed counting error, typo in variable name and list population logic

--- number_of_ways.py
+++ number_of_ways.py
@@ -20,27 +20,32 @@ def numberOfWays(startPos: int, endPos: int, k: int) -> int:
 
     # loop k times
     for i in tqdm(range(k)):
+        if i > 0:
+            paths = new_paths
+        new_paths = []
         for path in paths:
             new_path = path.copy()
             last_position = new_path[-1]
 
             # exist fast if not going to make to end
-            if endPos - last_position > (k - i - 1):
+            if endPos - last_position > (k - i):
                 continue
+
             # path that goes to the left
             new_path_left = new_path + [last_position - 1]
 
             # path that goes to the right
-            new_path_right = new_path + [last_position - 1]
+            new_path_right = new_path + [last_position + 1]
 
             # add paths to the left and right
-            paths.append(new_path_left)
-            paths.append(new_path_right)
+            new_paths.append(new_path_left)
+            new_paths.append(new_path_right)
 
+    paths = new_paths
     num_ways = 0
     for path in paths:
         if path[-1] == endPos:
-            new_ways += 1
+            num_ways += 1
     return num_ways
 
 
